aff4b047640774d45bb76164ce907b644fa53342
Josh Jancula <josh@jancula.com>
2018-02-21 02:36:56 +0000

    images

--- public/script.js
+++ public/script.js
@@ -8,62 +8,72 @@
 			{
 				question: "In season 5 who did the Armed Forces in Afganistan mistake a goat for?",
 				choices: ["Stevie Nicks", "Oprah Winfrey", "Madonna", "Mike krzyzewski"],
-				correctNum: 0
+				correctNum: 0,
+				answerImage: "http://www.southpark-online.nl/images/episodeclips/2536.jpg"
 			},
 
 			{
 				question: "What is Stans dads name?",
 				choices: ["Gerald", "Randy", "Jimbo", "Philip"],
-				correctNum: 1
+				correctNum: 1,
+				answerImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnj7yCW7tHFb_wWYrM4ieKwqrk4mOaxlpAxt8wjhC2y1eiiSTC"
 			},
 
 			{
 				question: "In which season did Jennifer Anniston make a guest appearance?",
 				choices: ["Season 8", "Season 4", "Season 12", "Season 3"],
-				correctNum: 3
+				correctNum: 3,
+				answerImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIujx9kTOyXYwhYgmvMHnv0sKZTLP9cpvQZ8mKJZ4Ucp9eUBVth6DAOxXv"
 			},
 
 			{
 				question: "In season 12 what were the Peruvian flute bands protecting the world from?",
-				choices: ["Satan", "Guinea Pigs", "ISIS", "The Gulgamacks"],
-				correctNum: 1
+				choices: ["Satan", "Giant Guinea Pigs", "ISIS", "The Gulgamacks"],
+				correctNum: 1,
+				answerImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF2LXQOpxSZAVhdNcVgCQ6hn02DWoVvtNJlrjpsG0lBg41kclFkw"
 			},
 
 			{
 				question: "Who is Mysterion?",
 				choices: ["Kyle", "Jimmy", "Kenny", "Butters"],
-				correctNum: 2
+				correctNum: 2,
+				answerImage: "https://vignette.wikia.nocookie.net/spsot/images/5/5b/Mysterion_tfbw.png/revision/latest?cb=20171020201046"
 			},
 
 			{
 				question: "Who was \"Super Cereal\" about Man-Bear-Pig?",
 				choices: ["Al Gore", "Ted Nugent", "Michael Jackson", "Officer Barbrady"],
-				correctNum: 0
+				correctNum: 0,
+				answerImage: "http://4.images.southparkstudios.com/blogs/southparkstudios.com/files/2014/07/1006-half-bear.jpg?quality=0.8"
 			},
 
 
 			{
 				question: "In season 8 which celebrity tried to buy Butters?",
 				choices: ["Lindsey Lohan", "Brittany Spears", "Paris Hilton", "Angelina Jolie"],
-				correctNum: 2
+				correctNum: 2,
+				answerImage: "https://vignette.wikia.nocookie.net/villains/images/9/96/Chi-parishilton-southpark.jpg/revision/latest?cb=20141230200947"
 			},
 
 			{
 				question: "What was the name of the organization Chef left South Park to join?",
 				choices: ["Antifa", "The Super Adventure Club", "The Marines", "The Peace Corp"],
-				correctNum: 1
+				correctNum: 1,
+				answerImage: "https://vignette.wikia.nocookie.net/villains/images/4/40/The_Super_Adventure_Club.jpg/revision/latest?cb=20131009164645"
 			},
 
 			{
 				question: "Who Replaced Principal Victoria in season 19?",
 				choices: ["Principal Belding", "Principal Gambi", "PC Principal", "Principal Russel"],
-				correctNum: 2
+				correctNum: 2,
+				answerImage: "https://vignette.wikia.nocookie.net/southpark/images/3/3e/Pc-principal.png/revision/latest?cb=20171221010050"
 			},
 
 			{
 				question: "In season 3 which band guest starred for the Halloween special?",
 				choices: ["Primus", "Korn", "Limp Bizkit", "Nickelback"],
-				correctNum: 1
+				correctNum: 1,
+				answerImage: "https://vignette.wikia.nocookie.net/southpark/images/7/78/Korn.png/revision/latest?cb=20161209151611"
 			},
 
 
@@ -178,22 +188,26 @@
 		function wasCorrect() {
 				$(".buttonAnswers").hide();
 				$("#clock").hide();
-				$("#currentQuestion").html("<h3> Correct!</h3>");
+				$("#currentQuestion").html("<h3> Correct!</h3>" + "<br>" +
+					"<img src=" + questions[questionIndex].answerImage + " class='answer'>");
 		}
 		
 		function wasWrong(){
 				$(".buttonAnswers").hide();
 				$("#clock").hide();
-				$("#currentQuestion").html("<h3>Wrong!</h3>" + "<h5>The correct answer is " + 
-				questions[questionIndex].choices[questions[questionIndex].correctNum] + "</h5>"
+				$("#currentQuestion").html("<h3>Wrong!</h3>" + "<h5>The correct answer is " + "<br>" +
+				questions[questionIndex].choices[questions[questionIndex].correctNum] + "</h5>" +
+					"<img src=" + questions[questionIndex].answerImage + " class='answer'>"
+				
 				);
 		}
 		
 			function outOfTime(){
 				$("#clock").hide();
 				$(".buttonAnswers").hide();
-				$("#currentQuestion").html("<h3>You Are Out Of Time!</h3>" + "<h5>The correct answer is " + 
-				questions[questionIndex].choices[questions[questionIndex].correctNum] + "</h5>"
+				$("#currentQuestion").html("<h3>You Are Out Of Time!</h3>" + "<h5>The correct answer is " + "<br>" +
+				questions[questionIndex].choices[questions[questionIndex].correctNum] + "</h5>" +
+						"<img src=" + questions[questionIndex].answerImage + " class='answer'>"
 				);
 		}
 
